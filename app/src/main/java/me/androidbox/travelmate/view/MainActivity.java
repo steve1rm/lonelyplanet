69cc547c88ff6a20edc0bcd3c4d61009c1c03d03
Steve <steve1.rm@gmail.com>
2016-02-21 19:38:20 +0700

    Added media files for MainActivity different cities

---
+++ app/src/main/java/me/androidbox/travelmate/view/MainActivity.java
@@ -0,0 +1,23 @@
+package me.androidbox.travelmate.view;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.androidbox.travelmate.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.main_fragment_container, new MainFragment(), "MainFragment");
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        }
+    }
+}
