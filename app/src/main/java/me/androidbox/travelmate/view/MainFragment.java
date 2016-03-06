d4cdbe2f07c4398c5a099d4997cc296466a4d0f4
Steve <steve1.rm@gmail.com>
2016-03-07 00:10:11 +0700

    Working on translation animation for moving the views

--- app/src/main/java/me/androidbox/travelmate/view/MainFragment.java
+++ app/src/main/java/me/androidbox/travelmate/view/MainFragment.java
@@ -1,14 +1,20 @@
 package me.androidbox.travelmate.view;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.transition.Scene;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import me.androidbox.travelmate.R;
 
@@ -16,6 +22,8 @@ import me.androidbox.travelmate.R;
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
+    private static final String TAG = MainFragment.class.getSimpleName();
+
     private Scene mToolbar_expanded;
     private Scene mToolbar_collapsed;
 
@@ -25,12 +33,46 @@ public class MainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.cheesequare, container, false);
+        final View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         final Toolbar toolbar = (Toolbar)view.findViewById(R.id.toolbar);
         final AppCompatActivity appCompatActivity = (AppCompatActivity)getActivity();
         appCompatActivity.setSupportActionBar(toolbar);
+
+        final ImageView ivLogo = (ImageView)view.findViewById(R.id.ivLogo);
+        final ImageView ivSearch = (ImageView)view.findViewById(R.id.ivSearch);
+        final NestedScrollView nsvCities = (NestedScrollView)view.findViewById(R.id.nsvCities);
+        final TextView tvTitle = (TextView)view.findViewById(R.id.tvTitle);
+
+        tvTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Animation animation= AnimationUtils.loadAnimation(getActivity(), R.anim.scaledown_logo);
+                ivLogo.startAnimation(animation);
+
+                android.support.v7.widget.Toolbar toolbarLayout = (android.support.v7.widget.Toolbar)view.findViewById(R.id.toolbar);
+                android.support.v7.widget.Toolbar.LayoutParams layoutParams = (android.support.v7.widget.Toolbar.LayoutParams)ivSearch.getLayoutParams();
+
+                int top = toolbarLayout.getTop();
+                int left = toolbarLayout.getLeft();
+                int bottom = toolbarLayout.getBottom();
+                int right = toolbarLayout.getRight();
+
+                final TranslateAnimation translateAnimation = new TranslateAnimation(left, right, top, bottom);
+                translateAnimation.setDuration(1000);
+                ivSearch.startAnimation(translateAnimation);
+            }
+        });
+
+        nsvCities.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                Log.d(TAG, "onScrollChange");
+            }
+        });
 
 /*
         mToolbar_expanded = Scene.getSceneForLayout((ViewGroup)view.findViewById(R.id.scene_root), R.layout.toolbar_scene_expanded, getActivity());
