8f323a5552589c3c1b42a3dfbe06effb57adcab3
steve <steve@sun-system.com>
2016-03-10 17:59:18 +0700

    testing

--- app/src/main/java/me/androidbox/travelmate/view/MainActivity.java
+++ app/src/main/java/me/androidbox/travelmate/view/MainActivity.java
@@ -2,7 +2,15 @@ package me.androidbox.travelmate.view;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import me.androidbox.travelmate.R;
 
@@ -25,34 +33,76 @@ public class MainActivity extends AppCompatActivity {
 
     /* noelseesleon */
     /* Noel sees leon. */
-    public static boolean isPalindrome(String str) {
-        char[] strRev = new char[str.length()];
-        char[] strOrg = str.toCharArray();
+    public static boolean isPalindrome(String s) {
+        /* Clean the string of all non-words */
+        final String strOrgClean = s.replaceAll("\\W", "");
+        char[] strRev = new char[strOrgClean.length()];
 
-        /* put in reverse order */
-        for(int i = str.length(); i == 0; i--) {
-            strRev[i] = str.charAt(i);
+        /* put string in reverse order */
+        for(int i = strOrgClean.length() - 1; i > -1; i--) {
+            /* Ignore all spaces and force stops */
+            strRev[i] = strOrgClean.charAt(i);
         }
 
         boolean isPlaindine = false;
-
-        /* compare each one end points */
-        for(int i = 0; i < str.length(); i++) {
-            if(strOrg[i] != '.' || strOrg[i] != ' ') {
-                if (strOrg[i] == strRev[i]) {
-                    isPlaindine = true;
-                }
-                else {
-                    isPlaindine = false;
-                    break;
-                }
-            }
+        /* Check by doing a simple string comparison */
+        if(strOrgClean.equalsIgnoreCase(String.valueOf(strRev))) {
+            isPlaindine = true;
         }
 
         return isPlaindine;
       //  throw new UnsupportedOperationException("Waiting to be implemented.");
     }
 
+
+    public int[] removeDuplicates(int[] a) {
+        /*
+          Please implement this method to
+          remove all duplicates from the original array. Retain the order of the elements and
+          always retain the first occurrence of the duplicate elements.
+          For example, parameter: {2,1,2,3}, result: {2,1,3}
+         */
+
+        int[] sorted = new int[a.length];
+
+        List<Integer> org = new ArrayList<>();
+        for(int num : a) {
+            org.add(num);
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(Integer number : org) {
+            /* Check do we have this number already */
+            if(map.containsKey(number)) {
+                Integer count = map.get(number);
+                /* Increment how many times its been duplicated */
+                count++;
+                map.put(number, count);
+            }
+            else {
+                map.put(number, 0);
+            }
+        }
+
+        int i = 0;
+        /* Now add to a new array */
+        for(Map.Entry entry : map.entrySet()) {
+            /* Always add the first one */
+            if(i == 0) {
+                sorted[i] = (int) entry.getKey();
+                i++;
+                continue;
+            }
+
+            if(entry.getValue() == 0) {
+                sorted[i] = (int)entry.getKey();
+            }
+            i++;
+        }
+
+        return sorted;
+
+    }
 
     /*
 An anagram is a word formed from another by rearranging its letters, using all the original letters exactly once; for example, orchestra can be rearranged into carthorse.
@@ -63,5 +113,23 @@ For example, areAnagrams("momdad", "dadmom") should return true as arguments are
     public static boolean areAnagrams(String a, String b) {
         return true;
         // throw new UnsupportedOperationException("Waiting to be implemented.");
+    }
+
+    public int[] positiveNumbers() {
+        int[] a = new int[] {2, -41, -5, 3, 7, 43, 34, 0, 2};
+        List<Integer> sortedInt = new ArrayList<>();
+        Integer[] intsSorted = new Integer[a.length];
+
+        for(int number : a) {
+            if(number > 0) {
+                sortedInt.add(number);
+            }
+        }
+
+        Collections.sort(sortedInt);
+
+        sortedInt.toArray(intsSorted);
+
+        return null;
     }
 }
