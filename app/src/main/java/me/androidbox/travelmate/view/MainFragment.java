803bb56a617aabd23ef8487abae9ad5257b4fed6
steve <steve@sun-system.com>
2016-03-16 18:01:28 +0700

    Changed to using LinearLayout

--- app/src/main/java/me/androidbox/travelmate/view/MainFragment.java
+++ app/src/main/java/me/androidbox/travelmate/view/MainFragment.java
@@ -3,6 +3,7 @@ package me.androidbox.travelmate.view;
 import android.app.Fragment;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,6 +17,8 @@ import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import me.androidbox.travelmate.R;
@@ -25,6 +28,8 @@ import me.androidbox.travelmate.R;
  */
 public class MainFragment extends Fragment {
     private static final String TAG = MainFragment.class.getSimpleName();
+
+    private Toolbar toolbar;
 
     public MainFragment() {
         // Required empty public constructor
@@ -37,14 +42,14 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        final Toolbar toolbar = (Toolbar)view.findViewById(R.id.toolbar);
+        toolbar = (Toolbar)view.findViewById(R.id.toolbar);
         final AppCompatActivity appCompatActivity = (AppCompatActivity)getActivity();
         appCompatActivity.setSupportActionBar(toolbar);
 
         final ImageView ivLogo = (ImageView)view.findViewById(R.id.ivLogo);
         final ImageView ivSearch = (ImageView)view.findViewById(R.id.ivSearch);
         final ImageView ivSettings = (ImageView)view.findViewById(R.id.ivSettings);
-        final NestedScrollView nsvCities = (NestedScrollView)view.findViewById(R.id.nsvCities);
+        final ScrollView nsvCities = (ScrollView)view.findViewById(R.id.nsvCities);
         final TextView tvTitle = (TextView)view.findViewById(R.id.tvTitle);
 
         final DisplayMetrics displayMetrics = new DisplayMetrics();
@@ -60,33 +65,44 @@ public class MainFragment extends Fragment {
 
         final float rawPX20PValue = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20 ,getResources().getDisplayMetrics());
 
-
         tvTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "toolbar.getHeight(): " + toolbar.getHeight() + "toolbar.getMinimumHeight(): " + toolbar.getMinimumHeight());
+
                 final Animation animation= AnimationUtils.loadAnimation(getActivity(), R.anim.scaledown_logo);
                 ivLogo.startAnimation(animation);
 
                 ivSearch.animate()
                         .setInterpolator(new AccelerateInterpolator())
                         .translationX(-60)
-                        .translationY(-40)
+                        .translationY(0)
                         .setDuration(250)
                         .start();
 
                 ivSettings.animate()
                         .setInterpolator(new AccelerateInterpolator())
                         .translationX(60)
-                        .translationY(-40)
+                        .translationY(0)
                         .setDuration(250)
                         .start();
             }
         });
 
+        final LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams)toolbar.getLayoutParams();
+
+    
+
         nsvCities.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
             @Override
             public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                Log.d(TAG, "onScrollChange");
+                Log.d(TAG, "onScrollChange scrollX " + scrollX + " scrollY: " + scrollY);
+
+                if(scrollY > 0 && scrollY <= 30) {
+                    layoutParams.height = scrollY * 10;
+                    toolbar.setLayoutParams(layoutParams);
+                    toolbar.requestLayout();
+                }
             }
         });
 
