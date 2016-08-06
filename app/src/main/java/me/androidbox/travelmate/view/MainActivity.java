b187c91b47778a75266d766215ef17f8749a7429
Steve Mason <steve@androidbox.me>
2016-08-06 00:38:43 -0400

    having the transparent layout moves the images up

--- app/src/main/java/me/androidbox/travelmate/view/MainActivity.java
+++ app/src/main/java/me/androidbox/travelmate/view/MainActivity.java
@@ -2,15 +2,7 @@ package me.androidbox.travelmate.view;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import me.androidbox.travelmate.R;
 
